8ad4e775be1ca4510624d3140cdb2f1c02cd32c5
XiozZe <XiozZe.gamemail@gmail.com>
2015-08-07 16:41:35 +0200

    Update XSUS.user.js

--- XSUS.user.js
+++ XSUS.user.js
@@ -5,7 +5,7 @@
 // @version        10.0
 // @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSML.js
 // @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/SupplyXioStock.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XioScript-10.0.user.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCF.js
 // @include        http://*virtonomic*.*/*/*
 // @exclude        http://virtonomics.wikia.com*
 // ==/UserScript==
