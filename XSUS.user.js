7948c1ead54b4b82e4be06a90a3ea24ac5253f37
XiozZe <XiozZe.gamemail@gmail.com>
2015-08-08 09:42:55 +0200

    Debug4

--- XSUS.user.js
+++ XSUS.user.js
@@ -3,17 +3,17 @@
 // @namespace      Virtonomics
 // @description    XioScript using XioLibraries
 // @version        10.0
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCF.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSML.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/SupplyXioStock.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/ZeroPrice.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/ToMyCompany.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/PriceOnStock.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/FullService.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/RequiredSalary.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/PrimeCostPrice.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/FullTraining.js
-// @require        https://rawgit.com/XiozZe/XioScript/master/XSCL/EquipmentImproveRepair.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCF.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSML.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/SupplyXioStock.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/ZeroPrice.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/ToMyCompany.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/PriceOnStock.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/FullService.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/RequiredSalary.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/PrimeCostPrice.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/FullTraining.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL/EquipmentImproveRepair.js
 // @include        http://*virtonomic*.*/*/*
 // @exclude        http://virtonomics.wikia.com*
 // ==/UserScript==
