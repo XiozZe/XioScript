8580c07bac9dc281bee0cb105b7d75fcf63a4727
XiozZe <XiozZe.gamemail@gmail.com>
2015-08-26 10:59:50 +0200

    Supply Zero&Required Bug Fix

--- XSCL/SupplyRequired.js
+++ XSCL/SupplyRequired.js
@@ -1,29 +1,33 @@
 XSCL.push({
-    row: "Production",
+    row: "Supply",
     name: "Required Supply",
-    description: "For the selected subdivisions on the main page, set the supply equal to the amount they require.",
+    description: "The stock of selected production subdivisions will have their supply set equal to the amount they require. Beware of the fact that each raw material has to have exactly one supplier: zero, two or more than two will give strange results, if any at all.",
     code: function(){
 
-        xcMain(["workshop"]);
+		xcMain(["mill", "workshop"]);
+	
+		xlist.push(function(){
+			for(var i = 0; i < xvar.main.xcId.length; i++){
+				xcGet("supplyGet", xvar.realm+"/main/unit/view/"+xvar.main.xcId[i]+"/supply");
+			}
+		});		
 
-        xlist.push(function(){
-            for ( var i = 0; i < xvar.main.xcId.length; i++ ){
-                xcGet( "supplyGet" , xvar.realm + "/main/unit/view/" + xvar.main.xcId[i] + "/supply" );       
-            }
-        }
-
-
-        xlist.push(function(){
-            for ( var i = 0; i < xvar.main.xcId.length; i++ ){
-                xvar.play.required = [];
-                for ( var j = 0 < xvar.supplyGet[i].required.length; j++){
-                    xvar.play.required.push( xvar.supplyGet[i].required[j] );
-                }
-                xcPost( "supplyPost" , xvar.supplyGet[i] , [["parcel", xvar.play.required]], "edit" );       
-            }
-        }
-
-        xcList();
+		xlist.push(function(){
+			for(var i = 0; i < xvar.main.xcId.length; i++){	
+				xvar.play.supply = [];
+				if(xvar.supplyGet[i].parcel){
+					for(var j = 0; j < xvar.supplyGet[i].parcel.length; j++){
+						xvar.play.supply.push( xvar.supplyGet[i].required[j] );
+					}
+					xcPost("supplyPost", xvar.supplyGet[i], [["parcel", xvar.play.supply]], "edit");
+				}
+				else{
+					console.log("This subdivision has no suppliers: "+xvar.main.xcId[0]);
+				}
+			}
+		});
+		
+		xcList();
 
     }
 });
