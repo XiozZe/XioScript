c3f0329b901ec00208b28e1379dad28a91908d03
XiozZe <XiozZe.gamemail@gmail.com>
2015-08-28 21:44:30 +0200

    Update trial instead of master

--- XSUS.user.js
+++ XSUS.user.js
@@ -2,13 +2,13 @@
 // @name           XioScript
 // @namespace      Virtonomics
 // @description    XioScript using XioLibraries
-// @version        10.0.12
+// @version        10.0.13
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
 // @require        http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCF.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSML.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSEL.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/Update-trial/XSCF.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/Update-trial/XSML.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/Update-trial/XSCL.js
+// @require        https://raw.githubusercontent.com/XiozZe/XioScript/Update-trial/XSEL.js
 // @resource       jQuiCss     https://raw.githubusercontent.com/XiozZe/XioScript/master/jquery-ui.css
 // @resource       myCss       https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCSS.css
 // @include        http://*virtonomic*.*/*/*
