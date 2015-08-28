2a8b20515c87b543cffba9c008cbc1324e08d527
XiozZe <XiozZe.gamemail@gmail.com>
2015-08-28 13:39:38 +0200

    Update XSCF.js

--- XSCF.js
+++ XSCF.js
@@ -8,6 +8,7 @@ console.log("extra consolelog2");
 console.log("extra consolelog3");
 console.log("extra consolelog4");
 console.log("extra consolelog5");
+console.log("extra consolelog6");
 
 function XioScript(){
 	
